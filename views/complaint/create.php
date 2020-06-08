050cf8efde140218b7867ccf4a45722346c5da8d
Reinout11erink <Reinout11erink@live.nl>
2020-06-08 14:53:16 +0200

    Update create.php

--- views/complaint/create.php
+++ views/complaint/create.php
@@ -7,14 +7,32 @@
         </div>
 
         <div class="form-group mb-3">
-            <label for="name">amount</label>
-            <input type="number" min="0" max="99999999" name="amount" value="<?php echo \Classes\Old::getOld('amount') ?>" class="form-control">
+            <label for="achternaam">achternaam</label>
+            <input type="text" name="achternaam" value="<?php echo \Classes\Old::getOld('achternaam') ?>" class="form-control">
         </div>
 
         <div class="form-group mb-3">
-            <label for="name">price</label>
-            <input type="text" name="price" value="<?php echo \Classes\Old::getOld('price') ?>" class="form-control">
+            <label for="email">email</label>
+            <input type="email" name="email" value="<?php echo \Classes\Old::getOld('email') ?>" class="form-control">
         </div>
+
+        <div class="form-group mb-3">
+            <label for="onderwerp">name</label>
+            <input type="text" name="onderwerp" value="<?php echo \Classes\Old::getOld('onderwerp') ?>" class="form-control">
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="name">prioriteit</label>
+            <input type="select" name="prioriteit" value="<?php echo \Classes\Old::getOld('prioriteit') ?>" class="form-control">
+        </div> 
+
+        <div class="form-group mb-3">
+            <label for="klacht">klacht</label>
+            <input type="text" name="klacht" value="<?php echo \Classes\Old::getOld('klacht') ?>" class="form-control">
+        </div>
+
+
+       
 
         <div class="form-group">
             <div class="text-right">
